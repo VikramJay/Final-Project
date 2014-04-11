3d452af7d535dbdf5a929daea9f87dd570ac2eaf
Vikram Jayashankar <Vikram.Jayashankar@gmail.com>
2014-04-10 19:10:38 -0500

    lol

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['DEBUG'] = True
 app.secret_key = 'WOW SUCH SECRET'
 db = SQLAlchemy(app)
 
