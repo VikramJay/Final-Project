1a7e29d49d1cf7e733de9ca6c05961d4bf479440
Vikram Jayashankar <Vikram.Jayashankar@gmail.com>
2014-04-10 19:27:29 -0500

    Revert "Revert "can i haz database""
    This reverts commit bda5e38f33bef2f39aef8ea569790b96472d9900.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
